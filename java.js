e598737f9bf4c2c641e676edee03062d67bceb07
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-28 13:48:27 -0600

    Added mouseover event listener to divs
    Added the mouseover event to color the div square
    grey when the user mouses over the div element.

--- java.js
+++ java.js
@@ -6,7 +6,13 @@ function createGrid(){
         for (let j = 1; j <= 16; j++) {
             const newDiv = document.createElement("div");
             newDiv.className = "grid";
+
+            newDiv.addEventListener("mouseover", function(){
+                newDiv.style.backgroundColor = "grey";
+            });
+
             container.appendChild(newDiv);
+        
         }
     }
 
