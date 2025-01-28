2a9e79dd92acd61cd08db584d4433194aef8ffa2
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-28 12:59:03 -0600

    Create a 16 by 16 square grid with flexbox
    Used flexbox to create a flex wrapping grid layout
    for the div element in the container. Adjusted the
    width of the main flex container to match
    the 16 by 16 layout. Defined the dimensions for
    the div squares we are using.

---
+++ java.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "grid";
+        container.appendChild(newDiv);
+    }
+}
