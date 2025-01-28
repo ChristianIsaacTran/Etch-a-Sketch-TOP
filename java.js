85d37bc25b23e8b52fcedc7bd50d71f1aa4d4dbe
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-28 14:14:36 -0600

    Added grid removal and prompt for new grid button
    Added a function that removes the existing grid
    from the page by calling querySelectorAll HTML
    elements with the class tag "grid", then iterating
    through that reference list and removing them from
    the container. Need to rescale the container so
    the grid displays correctly.

--- java.js
+++ java.js
@@ -1,23 +1,47 @@
+const defaultSize = 16;
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
+const btnNewGrid = document.createElement("button");
+btnNewGrid.textContent = "Generate New Grid";
+btnNewGrid.style.margin = "10px";
 
+btnNewGrid.addEventListener("click", createNewGrid);
 
-function createGrid(){
-    for (let i = 1; i <= 16; i++) {
-        for (let j = 1; j <= 16; j++) {
+body.insertBefore(btnNewGrid, container);
+
+function createGrid(size) {
+    for (let i = 1; i <= size; i++) {
+        for (let j = 1; j <= size; j++) {
             const newDiv = document.createElement("div");
             newDiv.className = "grid";
 
-            newDiv.addEventListener("mouseover", function(){
+            newDiv.addEventListener("mouseover", function () {
                 newDiv.style.backgroundColor = "grey";
             });
 
             container.appendChild(newDiv);
-        
+
         }
     }
-
     console.log("GRID CREATED");
 }
 
-createGrid();
+function removeGrid(inputSize) {
+    const divList = document.querySelectorAll(".grid");
+    divList.forEach(div => {
+        container.removeChild(div);
+    });
+}
+
+function createNewGrid() {
+    let inputSize = prompt("Please enter in the square grid size: ", "16");
+    console.log(inputSize);
+    
+    removeGrid(inputSize);
+    createGrid(inputSize);
+}
+
+
+
+createGrid(defaultSize);
 
