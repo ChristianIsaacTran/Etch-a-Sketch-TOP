614366fdc957680150165c231a8412c31da49c16
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-28 13:17:10 -0600

    Made the grid generation into function createGrid

--- java.js
+++ java.js
@@ -1,9 +1,17 @@
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 16; i++) {
-    for (let j = 1; j <= 16; j++) {
-        const newDiv = document.createElement("div");
-        newDiv.className = "grid";
-        container.appendChild(newDiv);
+
+function createGrid(){
+    for (let i = 1; i <= 16; i++) {
+        for (let j = 1; j <= 16; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.className = "grid";
+            container.appendChild(newDiv);
+        }
     }
+
+    console.log("GRID CREATED");
 }
+
+createGrid();
+
